5dd96d6c285ecba04952b6d6ef62045a4d53b602
wangyao <13564118478@163.com>
2019-08-09 10:37:40 +0800

    减少冗余

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# gzmitm
+/usr/local/bin/mitmdump --set block_global=false -s /code/start_script.py
+
+BROKER_URL = 'amqp://cic_admin:JYcxys@3030@{}:{}/yct_gz'.format(RABBITMQ_HOST,RABBITMQ_PORT)
+
+yct_gz/to_create
+
--- handle_data/main.py
+++ handle_data/main.py
@@ -1,10 +1,23 @@
 # -*- coding:utf-8 -*-
-from handle_data.tasks import *
+# from handle_data.tasks import
 # print('hello')
+from handle_data import celery_app
+
+
 def handle_data(data_str):
     # res = chain(to_product.s(data_str), to_analysis.s(), to_consume.s())()
 
     # 插入一条pickle后的数据，返回记录的id res1
-    res = to_create.apply_async(args=[data_str], retry=True, queue='to_create', immutable=True)
+    to_create.apply_async(args=[data_str], retry=True, queue='to_create', immutable=True)
     # res = to_create(data_str)
     # print(res)
+
+
+@celery_app.task(name='to_create')
+def to_create(data):
+    '''解析出所有的退回数据,将pickle的数据做解析'''
+    pass
+    # if data:
+    #     order_number = str(random.random())
+    #     REDIS_GZ.set(order_number, data, ex=3600)
+    #     to_analysis.apply_async(args=[order_number], retry=True, queue='to_analysis', immutable=True)
